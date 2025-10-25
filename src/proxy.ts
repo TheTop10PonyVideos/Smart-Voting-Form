2ed512c2d18006a1c4def201c5f6f6187182f98f
Brambles-cat <gael.alejos05@gmail.com>
2025-10-25 09:20:05 -0700

    Update to NextJs 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server"
 // Ensures that a user cookie is either set or attempted to be set
 // The cookie value is present in the page raouters regardless of whether it was accepted or not
 // so accessing the cookie there should never return undefined
-export default function middleware(request: NextRequest) {
+export default function proxy(request: NextRequest) {
   const uid = request.cookies.get("uid")?.value
   const resp = NextResponse.next()
 
@@ -15,7 +15,7 @@ export default function middleware(request: NextRequest) {
   return resp
 }
 
-// Only running middleware for regular pages
+// Only running for regular pages
 export const config = {
   matcher: ["/((?!api|_next|favicon.ico|.well-known).*)"],
 }
